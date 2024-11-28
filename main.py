31a307047845d170d23a03079cde581723d041fe
kokiludak <konstantin.pasic@gmail.com>
2024-11-28 13:06:34 +0100

    Complete RequestUserInfo function and add main

--- main.py
+++ main.py
@@ -4,5 +4,13 @@ import requests
 
 def RequestUserInfo(user):
     ratingHistory = requests.get("https://codeforces.com/api/user.rating?" + user)
-    userSubmisisons = requests.get("https://codeforces.com/user.status?" + user)
+    userSubmissions = requests.get("https://codeforces.com/user.status?" + user)
+    return (ratingHistory, userSubmissions)
+
+
+if __name__ == "__main__":
+    print("Enter username: ")
+    user = input()
+    (ratingHistory, userSubmissions) = RequestUserInfo(user)
+    print(ratingHistory)
     
